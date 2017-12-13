ae8386b8bbfdcdff21dc72d4bffffd54b5ff6114
Erik Peterson <Erik.Exists@gmail.com>
2017-12-12 23:37:41 -0800

    added a raw version of minst loading - download_minst()

--- a_toys/datasets.py
+++ a_toys/datasets.py
@@ -14,7 +14,7 @@ from urllib.request import urlretrieve
 def load_mnist():
     partial_flatten = lambda x: np.reshape(x, (x.shape[0], np.prod(x.shape[1:])))
     one_hot = lambda x, k: np.array(x[:,None] == np.arange(k)[None, :], dtype=int)
-    train_images, train_labels, test_images, test_labels = get_mnist()
+    train_images, train_labels, test_images, test_labels = download_mnist()
     train_images = partial_flatten(train_images) / 255.0
     test_images = partial_flatten(test_images) / 255.0
     train_labels = one_hot(train_labels, 10)
@@ -32,7 +32,7 @@ def download(url, filename):
         urlretrieve(url, out_file)
 
 
-def get_mnist():
+def download_mnist():
     base_url = 'http://yann.lecun.com/exdb/mnist/'
 
     def parse_labels(filename):
