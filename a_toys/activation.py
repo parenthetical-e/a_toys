633df598f4cbdf98c98aa99886c853dd678c2c59
Erik Peterson <Erik.Exists@gmail.com>
2017-12-12 23:37:07 -0800

    added softmax activation

--- a_toys/activation.py
+++ a_toys/activation.py
@@ -7,3 +7,13 @@ def relu(x):
 
 def sigmoid(x):
     return np.tanh(x)
+
+
+def _logsumexp(X, axis, keepdims=False):
+    max_X = np.max(X)
+    return max_X + np.log(
+        np.sum(np.exp(X - max_X), axis=axis, keepdims=keepdims))
+
+
+def softmax(x):
+    return x - _logsumexp(x, axis=1, keepdims=True)
